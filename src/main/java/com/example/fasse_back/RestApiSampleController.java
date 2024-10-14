c5d0183a93cf642d8d5d6ffbd2e01e301ebeb376
kachi <kachisvp@gmail.com>
2024-10-14 17:00:23 +0900

    SpringBoot template commit.

---
+++ src/main/java/com/example/fasse_back/RestApiSampleController.java
@@ -0,0 +1,25 @@
+package com.example.fasse_back;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class RestApiSampleController {
+
+    @GetMapping("rest-api-sample")
+    public String restApiSample() {
+        Map<String, String> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("test");
+        map.put("test", "value");
+        return map.toString();
+    }
+    
+}
