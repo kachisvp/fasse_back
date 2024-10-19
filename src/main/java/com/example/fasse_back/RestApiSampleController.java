62b0d3fe87c09c500cf1daf90996565dc61ba76b
kachi <kachisvp@gmail.com>
2024-10-19 10:51:34 +0900

    [application.yaml]に変更

--- src/main/java/com/example/fasse_back/RestApiSampleController.java
+++ src/main/java/com/example/fasse_back/RestApiSampleController.java
@@ -5,10 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApiSampleController {
