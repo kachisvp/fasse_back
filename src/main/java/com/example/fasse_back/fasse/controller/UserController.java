52c6ecb65686774ea57780ef9d05c66953c773b4
kachi <kachisvp@gmail.com>
2024-12-08 09:07:53 +0900

    Jacksonを使ってJSON形式で値を返す様に修正

--- src/main/java/com/example/fasse_back/fasse/controller/UserController.java
+++ src/main/java/com/example/fasse_back/fasse/controller/UserController.java
@@ -1,20 +1,27 @@
 package com.example.fasse_back.fasse.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.fasse_back.user.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private static ObjectMapper objectMapper = new ObjectMapper();
     private final UserService userService;
 
     @GetMapping("/users")
-    public String findAll() {
-        return userService.findAll().toString();
+    public String findAll() throws JsonProcessingException {
+        log.info(UserController.class.toString() + ": findAll()");
+
+        return objectMapper.writeValueAsString(userService.findAll());
     }
-    
 }
